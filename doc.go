18d8f64805d9dbeefd38306bdeb9f8f6d7e11da6
Kare Nuorteva <kare.nuorteva@streamr.com>
2018-10-12 15:50:12 +0300

    Fix lint nit on windows about doc comment
    C:\Users\travis\gopath\src\github.com\streamr-dev\go-streamr-client\doc.go:19:1: package comment is detached; there should be no blank lines between it and the package statement

--- doc.go
+++ doc.go
@@ -1,20 +1,17 @@
-/*
-Package streamr provides a client for Streamr API.
-
-Usage:
-	import streamr "github.com/streamr-dev/go-streamr-client"
-
-Construct a new Streamr Client, then use the various services on the client to
-access different parts of the Streamr API. For example:
-
-	var (
-		client *streamr.Client
-		err error
-	)
-	client, err = streamr.NewClient("streamr client authentication key")
-
-	// stream data to streamr.com
-	var response *streamr.Response
-	response, err = client.Data.ProduceToStream("stream id", mydata)
-*/
+// Package streamr provides a client for Streamr API.
+// Usage:
+//	import streamr "github.com/streamr-dev/go-streamr-client"
+//
+// Construct a new Streamr Client, then use the various services on the client to
+// access different parts of the Streamr API. For example:
+//
+//	var (
+//		client *streamr.Client
+//		err error
+//	)
+//	client, err = streamr.NewClient("streamr client authentication key")
+//
+//	// stream data to streamr.com
+//	var response *streamr.Response
+//	response, err = client.Data.ProduceToStream("stream id", mydata)
 package streamr
